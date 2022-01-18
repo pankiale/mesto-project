952be6946eff50964a6a9dd3c0f5fb1c1e0d562b
Alexander Pankin <alexander.g.pankin@gmail.com>
2022-01-18 18:10:23 +0100

    script refactor

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,39 @@
-let content = document.querySelector('.content');
-let element = content.querySelector('.element');
-let photoContainer = document.querySelector('.elements');
-let addButton = document.querySelector('.profile__add-button');
-let editButton = document.querySelector('.profile__edit-button');
-let closeButtonPhoto = document.querySelector('.popup__close-button_photo-card');
-let closeButtonPhotoScaled = document.querySelector('.popup__close-button_photo-scaled');
-let closeButtonAvatar = document.querySelector('.popup__close-button_avatar');
-let submitProfileForm = document.querySelector('.form_prof');
-let submitPhotoForm = document.querySelector('.form_photo');
-let popupAvatar = document.querySelector('.popup_type_avatar');
-let popupPhoto = document.querySelector('.popup_type_photo-card');
-let popupPhotoScaled = document.querySelector('.popup_type_photo-scaled');
-let profileName = document.querySelector('.profile__title');
-let profileJob = document.querySelector('.profile__subtitle');
-let nameInput = document.querySelector('.form__item_prof_title');
-let jobInput = document.querySelector('.form__item_prof_subtitle');
+const content = document.querySelector('.content');
+const element = content.querySelector('.element');
+const photoContainer = document.querySelector('.elements');
+const buttonAddPhoto = document.querySelector('.profile__add-button');
+const buttonEditProfile = document.querySelector('.profile__edit-button');
+const buttonClosePhoto = document.querySelector('.popup__close-button_photo-card');
+const buttonClosePhotoScaled = document.querySelector('.popup__close-button_photo-scaled');
+const buttonCloseProfile = document.querySelector('.popup__close-button_avatar');
+const buttonSubmitProfile = document.querySelector('.form_prof');
+const buttonSubmitPhoto = document.querySelector('.form_photo');
+const popupProfile = document.querySelector('.popup_type_avatar');
+const popupPhoto = document.querySelector('.popup_type_photo-card');
+const popupPhotoScaled = document.querySelector('.popup_type_photo-scaled');
+const profileName = document.querySelector('.profile__title');
+const profileJob = document.querySelector('.profile__subtitle');
+const nameInput = document.querySelector('.form__item_prof_title');
+const jobInput = document.querySelector('.form__item_prof_subtitle');
+const photoName = document.querySelector('.form__item_photo_title');
+const photoLink = document.querySelector('.form__item_photo_link');
+const photoTemplate = document.querySelector('#element-template').content;
+let photoCard = undefined;
 
 
 /*  задаем фунцию для добавления фотокарточки*/
-function addPhoto(photoName, photoLink) {
-  const photoTemplate = document.querySelector('#element-template').content;
-  const photoCard = photoTemplate.querySelector('.element').cloneNode(true);
+function createPhoto(photoName, photoLink) {
+  photoCard = photoTemplate.querySelector('.element').cloneNode(true);
   photoCard.querySelector('.element__title').textContent = photoName;
   photoCard.querySelector('.element__image').src = photoLink;
   photoCard.querySelector('.element__image').alt = photoName;
-  photoContainer.prepend(photoCard);
-}
+  return photoCard;
+};
+
+/*  задаем фунцию для отрисовки фотокарточки*/
+function renderPhoto(photo) {
+  photoContainer.prepend(photo);
+};
 
 /*  проверяем что контейнер элементс пустой и заполняем его 6 карточками.
 цикл обратный чтобы сделать фунцию добавления карточи универсальной через prepend*/
@@ -33,14 +41,15 @@ if (element === null) {
   for (let i = initialCards.length - 1; i > -1; i -= 1) {
     const photoName = initialCards[i].name;
     const photoLink = initialCards[i].link;
-    addPhoto(photoName, photoLink);
+    createPhoto(photoName, photoLink);
+    renderPhoto(photoCard);
   }
 };
 
 /*  задаем фунцию открытия попап только для попап профиля забираем данные для формы из дом*/
 function openPopUp(popupName) {
   popupName.classList.add('popup_open');
-  if (popupName === popupAvatar) {
+  if (popupName === popupProfile) {
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
   }
@@ -49,7 +58,7 @@ function openPopUp(popupName) {
 /*  задаем фунцию закрытия попап только для попап профиля обнуляем данные в форме*/
 function closePopUp(popupName) {
   popupName.classList.remove('popup_open');
-  if (popupName === popupAvatar) {
+  if (popupName === popupProfile) {
     nameInput.value = '';
     jobInput.value = '';
   }
@@ -60,29 +69,28 @@ function profileFormSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  closePopUp(popupAvatar);
+  closePopUp(popupProfile);
 };
 
 /*обрабатывает фото карточку - забирает имя и ссылку из формы, передает их функции addphoto
 (добавление фотокарточки на страницу) и обнуляет поля имя и ссылка в форме затем вызывает closePopUp*/
 function photoFormSubmitHandler(evt) {
   evt.preventDefault();
-  let photoName = document.querySelector('.form__item_photo_title');
-  let photoLink = document.querySelector('.form__item_photo_link');
-  addPhoto(photoName.value, photoLink.value);
+  createPhoto(photoName.value, photoLink.value);
+  renderPhoto(photoCard);
   photoName.value = '';
   photoLink.value = '';
   closePopUp(popupPhoto);
 };
 
 /*  слушатели событий кроме работы с карточками*/
-editButton.addEventListener('click', openPopUp.bind(null, popupAvatar));
-addButton.addEventListener('click', openPopUp.bind(null, popupPhoto))
-closeButtonAvatar.addEventListener('click', closePopUp.bind(null, popupAvatar));
-closeButtonPhoto.addEventListener('click', closePopUp.bind(null, popupPhoto));
-closeButtonPhotoScaled.addEventListener('click', closePopUp.bind(null, popupPhotoScaled));
-submitProfileForm.addEventListener('submit', profileFormSubmitHandler);
-submitPhotoForm.addEventListener('submit', photoFormSubmitHandler);
+buttonEditProfile.addEventListener('click', openPopUp.bind(null, popupProfile));
+buttonAddPhoto.addEventListener('click', openPopUp.bind(null, popupPhoto))
+buttonCloseProfile.addEventListener('click', closePopUp.bind(null, popupProfile));
+buttonClosePhoto.addEventListener('click', closePopUp.bind(null, popupPhoto));
+buttonClosePhotoScaled.addEventListener('click', closePopUp.bind(null, popupPhotoScaled));
+buttonSubmitProfile.addEventListener('submit', profileFormSubmitHandler);
+buttonSubmitPhoto.addEventListener('submit', photoFormSubmitHandler);
 
 /* один слушатель чтобы избежать большого числа слушателей если пользователь создаст много фотокарточек
 ставим лайки  удаляем карточки и открываем карточки на большой экран*/
