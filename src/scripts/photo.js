bdd4a1143c77d6bf9956fb0195e51c8be196395f
pankiale <93738391+pankiale@users.noreply.github.com>
2022-03-28 12:06:18 +0200

    form reset

--- src/scripts/photo.js
+++ src/scripts/photo.js
@@ -9,6 +9,7 @@ const buttonClosePhoto = document.querySelector('.popup__close-button_photo-card
 const buttonClosePhotoScaled = document.querySelector('.popup__close-button_photo-scaled');
 const buttonSubmitPhoto = document.querySelector('.form_photo');
 const popupPhoto = document.querySelector('.popup_type_photo-card');
+const formReset = popupPhoto.querySelector('.form');
 const popupPhotoScaled = document.querySelector('.popup_type_photo-scaled');
 const photoName = document.querySelector('.form__item_photo_title');
 const photoLink = document.querySelector('.form__item_photo_link');
@@ -17,17 +18,13 @@ const submitButton = popupPhoto.querySelector('.form__save-button');
 function submitPhotoForm(evt) {
   evt.preventDefault();
   renderPhoto(createPhoto(photoName.value, photoLink.value));
-  photoName.reset();
-  photoLink.reset();
   closePopUp(popupPhoto);
 };
 
 export function setEventListenersPhoto () {
 buttonAddPhoto.addEventListener('click', () => {
-
   openPopUp(popupPhoto);
-  photoName.reset();
-  photoLink.reset();
+  formReset.reset();
   const errorMessages = popupPhoto.querySelectorAll('.form__error');
   errorMessages.forEach(error => error.textContent = '');
   const errorInputs = popupPhoto.querySelectorAll('.form__item_error');
